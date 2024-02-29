8c75805243c181368e9cdfded2c938883e5682f2
Brian Vo <brianhvo02@gmail.com>
2024-02-28 20:55:43 -0800

    rewrote docker scripts

--- wasm/scripts/docker_build.sh
+++ wasm/scripts/docker_build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+set -e
+
+# Mac Docker users need to change file sharing from VirtioFS to gRPC FUSE
 docker run -dt --name bdinfo-build -v $(pwd):/app -w /app/wasm debian 
 docker exec -it bdinfo-build ./scripts/install_reqs.sh
 docker exec -it -u $(id -u):$(id -g) bdinfo-build ./scripts/build.sh
--- wasm/scripts/docker_compile.sh
+++ wasm/scripts/docker_compile.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-docker run --rm -v $(pwd):/src -u $(id -u):$(id -g) emscripten/emsdk \
+docker run --rm -v $(pwd):/app -u $(id -u):$(id -g) -w /app/wasm emscripten/emsdk \
     emcc -O3 --embind-emit-tsd interface.d.ts \
         -s FORCE_FILESYSTEM \
         -l workerfs.js -l embind \
-        -I ./wasm/build/libbluray/src  -o ./public/static/js/libbluray_web.js \
-        ./wasm/bluray.cpp ./wasm/build/libbluray/.libs/libbluray.a ./wasm/build/libxml2/.libs/libxml2.so ./wasm/build/freetype/build/libfreetype.a && \
+        -I ./build/libbluray/src  -o ../public/static/js/libbluray_web.js \
+        ./bluray.cpp ./build/libbluray/.libs/libbluray.a ./build/libxml2/.libs/libxml2.so ./build/freetype/build/libfreetype.a && \
     mv ./public/static/js/interface.d.ts ./src/types
